23b5f294146a13728bf25dd622c5a67b47a64a31
mustafa kemal mutlu <kemalmutlu121212@gmail.com>
2024-06-14 18:56:29 +0300

    services'ın controller'ı yazıldı avatar güncelleme hariç diğer test edildi

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,3 +27,198 @@ exports.register = async (req, res) => {
       .status(StatusCodes.INTERNAL_SERVER_ERROR);
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const user = await service.userService.login(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "giriş yapıldı",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "giriş yapılamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await service.userService.getAllUsers(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcılar bulundu",
+        data: users,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcılar bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+exports.updateAvatar = async (req, res) => {
+  try {
+    const user = await service.userService.updateAvatar(req, res);
+    return res
+      .status(StatusCodes.OK)
+      .json({
+        ...baseResponse,
+        message: "Kullanıcı avatarı güncellendi",
+        data: user,
+        timestamp: new Date(),
+      });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        ...baseResponse,
+        message: "Kullanıcı avatarı güncellenemedi",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await service.userService.getUserById(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "ID'ye kullancıcı getirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+exports.updatePassword = async (req, res) => {
+  try {
+    const user = await service.userService.UpdatePassword(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı şifresi başarılı şekilde değiştirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı şifresi değiştirilemedi ",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+exports.deleteUserById = async (req, res) => {
+  try {
+    const user = await service.userService.deleteUserById(req);
+    res
+    .status(StatusCodes.OK)
+    .json({
+      ...baseResponse,
+      message: "Kullanıcı başarılı şekilde silindi",
+      data: user,
+      timestamp: new Date(),
+    });
+  } catch (error) {
+    res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({
+      ...baseResponse,
+      message: "Kullanıcı silinemedi",
+      success: false,
+      error: true,
+      errorMessage: error.message,
+      timestamp: new Date(),
+    });
+  }
+};
+exports.updateUserById = async (req, res) => {
+  try {
+    const user = await service.userService.updateUserById(req);
+    res
+      .status(StatusCodes.OK)
+      .json({
+        ...baseResponse,
+        message: "Kullanıcı başarılı şekilde güncellendi.",
+        data: user,
+        timestamp: new Date(),
+      });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        ...baseResponse,
+        message: "Kullanıcı güncellenmedi.",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      });
+  }
+};
+
+exports.updateUserPin = async (req, res) => {
+  try {
+    const user = await service.userService.updateUserPin(req);
+    res
+      .status(StatusCodes.OK)
+      .json({
+        ...baseResponse,
+        message: "Kullanıcı PIN'i başarılı şekilde güncellendi.",
+        data: user,
+        timestamp: new Date(),
+      });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({
+        ...baseResponse,
+        message: "Kullanıcı PIN'i güncellenmedi.",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      });
+  }
+};
--- routers/user.router.js
+++ routers/user.router.js
@@ -2,6 +2,14 @@ const router = require("express").Router();
 const controller = require("../controllers/index");
 
 router.post("/register", controller.userController.register);
+router.post("/login", controller.userController.login);
+router.get("/getAllUsers", controller.userController.getAllUsers);
+router.get("/getUserById/:id", controller.userController.getUserById);
+router.put("/updateAvatar/:id", controller.userController.updateAvatar);
+router.put("/updatePassword/:id", controller.userController.updatePassword);
+router.delete("/deleteUserById/:id", controller.userController.deleteUserById);
+router.put("/updateUserById/:id", controller.userController.updateUserById); 
+router.put("/updateUserPin/:id", controller.userController.updateUserPin);
 
 module.exports = {
   user: router,
