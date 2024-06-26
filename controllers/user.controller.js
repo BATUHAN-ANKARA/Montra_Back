3b8af9639ef1a62525b01b18d067ef503729d32c
Batuhan Ankara <sertgumecbatuhan@gmail.com>
2024-06-26 17:27:03 +0300

    fixed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,3 +27,128 @@ exports.register = async (req, res) => {
       .status(StatusCodes.INTERNAL_SERVER_ERROR);
   }
 };
+
+exports.getUserByEmail = async (req, res) => {
+  try {
+    const user = await service.userService.getUserByEmail(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "Email ile kullancıcı getirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+
+exports.getUserByName = async (req, res) => {
+  try {
+    const user = await service.userService.getUserByName(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "İsim ile kullancıcı getirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+
+exports.getUserBySurname = async (req, res) => {
+  try {
+    const user = await service.userService.getUserBySurname(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "Soyisim ile kullancıcı getirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+
+exports.getUserByAge = async (req, res) => {
+  try {
+    const user = await service.userService.getUserByAge(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "Yaş ile kullancıcı getirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+
+exports.getUserByGender = async (req, res) => {
+  try {
+    const user = await service.userService.getUserByGender(req);
+    res
+      .json({
+        ...baseResponse,
+        message: "Cinsiyet ile kullancıcı getirildi",
+        data: user,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.OK);
+  } catch (error) {
+    res
+      .json({
+        ...baseResponse,
+        message: "kullanıcı bulunamadı",
+        success: false,
+        error: true,
+        errorMessage: error.message,
+        timestamp: new Date(),
+      })
+      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
